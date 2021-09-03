7cb17007b67174053e0911981ee0ab40c56135be
gkxotjd12312 <gkxotjd12312@naver.com>
2021-09-03 14:42:40 +0900

    first_test

---
+++ Assets/Scripts/유종인.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 유종인 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("a");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
